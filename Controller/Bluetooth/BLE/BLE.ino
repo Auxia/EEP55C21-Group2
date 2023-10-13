9dee9f4d056cf9e781c92e99dc20f2be122c5616
Auxia <sbhathal12@gmail.com>
2023-10-13 15:08:03 +0100

    Configured BLE for gyroscope transmission (Test)

--- Controller/Bluetooth/BLE/BLE.ino
+++ Controller/Bluetooth/BLE/BLE.ino
@@ -1,46 +1,118 @@
+#include <Arduino_LSM6DS3.h>
 #include <ArduinoBLE.h>
 
-BLEService myService("fff0");
-BLEIntCharacteristic myCharacteristic("fff1", BLERead | BLEBroadcast);
+// Define custom BLE service for position (read-only)
+BLEService posService("95ff7bf8-aa6f-4671-82d9-22a8931c5387");
+BLEFloatCharacteristic posX("95ff7bf8-aa6f-4671-82d9-22a8931c5387", BLERead);
+BLEFloatCharacteristic posY("f49caa00-17f8-4e92-b5fd-d27137ca4515", BLERead);
+BLEFloatCharacteristic posZ("84f9b003-6d14-44d7-8db1-d574d29c10c3", BLERead);
 
-// Advertising parameters should have a global scope. Do NOT define them in 'setup' or in 'loop'
-const uint8_t manufactData[4] = {0x01, 0x02, 0x03, 0x04};
-const uint8_t serviceData[3] = {0x00, 0x01, 0x02};
+// Define custom BLE service for LED management (read/write)
+BLEService ledService("daaac223-ea6d-411f-8d8e-32bfb46d4bad");
+BLEByteCharacteristic led("40369706-e126-4563-b7ae-3a34b45b3ab8", BLERead | BLEWrite);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+  // Initialize internal LED (for visual debugging)
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
-  if (!BLE.begin()) {
-    Serial.println("failed to initialize BLE!");
-    while (1);
+  // Initialize Serial connection
+  Serial.begin(115200);
+
+  // Initialize IMU
+  if (!IMU.begin()) {
+    // Failed to initialize IMU, blink the internal LED
+    Serial.println("Failed initializing IMU");
+    while (1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+    }
   }
 
-  BLE.setDeviceName("Nano 33 IoT");
+  // Initialize BLE
+  if(!BLE.begin()) {
+    // Failed to initialize BLE, blink the internal LED
+    Serial.println("Failed initializing BLE");
+    while (1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+    }
+  }
 
-  myService.addCharacteristic(myCharacteristic);
-  BLE.addService(myService);
+  // Set advertised local name and services UUID
+  BLE.setDeviceName("Arduino Nano 33 IoT");
+  BLE.setLocalName("BLE Experiment");
 
-  // Build scan response data packet
-  BLEAdvertisingData scanData;
-  // Set parameters for scan response packet
-  // scanData.setLocalName("Test enhanced advertising");
-  // Copy set parameters in the actual scan response packet
-  BLE.setScanResponseData(scanData);
+  posService.addCharacteristic(posX);
+  posService.addCharacteristic(posY);
+  posService.addCharacteristic(posZ);
+  BLE.addService(posService);
 
-  // Build advertising data packet
-  BLEAdvertisingData advData;
-  // Set parameters for advertising packet
-  advData.setManufacturerData(0x004C, manufactData, sizeof(manufactData));
-  advData.setAdvertisedService(myService);
-  advData.setAdvertisedServiceData(0xfff0, serviceData, sizeof(serviceData));
-  // Copy set parameters in the actual advertising packet
-  BLE.setAdvertisingData(advData);
+  ledService.addCharacteristic(led);
+  BLE.addService(ledService);
 
+  // Set default values for characteristics
+  posX.writeValue((float)0.0);
+  posY.writeValue((float)0.0);
+  posZ.writeValue((float)0.0);
+  led.writeValue(0);
+
+  // Start advertising
   BLE.advertise();
-  Serial.println("advertising ...");
 }
 
 void loop() {
-  BLE.poll();
+  // Listen for BLE
+  BLEDevice central = BLE.central();
+
+  // If a central is connected
+  if(central) {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+  }
+
+  // While central is still connected...
+  while(central.connected()) {
+    float x, y, z;
+    byte ledValue = 0x0;
+
+    // Read gyroscope values
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(x, y, z);
+    }
+
+    // Display gyroscope values on serial
+    Serial.print("Position ");
+    Serial.print(x);
+    Serial.print(",");
+    Serial.print(y);
+    Serial.print(",");
+    Serial.println(z);
+    Serial.print("LED is ");
+    Serial.println(ledValue);
+
+    // Write values on BLE
+    posX.writeValue(x);
+    posY.writeValue(y);
+    posZ.writeValue(z);
+    led.readValue(ledValue);
+
+    // Read values on BLE
+    if(ledValue == 0x0) {
+      digitalWrite(LED_BUILTIN, LOW);
+    } else {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    delay(1000);
+  }
+
+  // when the central disconnects, print it out:
+  Serial.print("Disconnected from central: ");
+  Serial.println(central.address());
+  delay(1000);
 }
