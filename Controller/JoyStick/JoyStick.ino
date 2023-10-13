792dd93f023543f07024e78be9d914fc68969d58
Auxia <sbhathal12@gmail.com>
2023-10-13 16:21:43 +0100

    Smoothened the input from joysticks

--- Controller/JoyStick/JoyStick.ino
+++ Controller/JoyStick/JoyStick.ino
@@ -1,4 +1,9 @@
+
 #include "HID-Project.h"
+
+#define deadRangeLow 512-75
+#define deadRangeHigh 512+75
+#define NUM_READINGS 30  // for example, 10 readings for averaging
 
 const int pinButton = 2;  // Change to your button's pin
 const int pinXAxis = A0;  // Change to your X-Axis pin
@@ -8,7 +13,20 @@ const int dialPin2 = 10; // Labeled "2" on encoder
 const int dialPin3 = 11; // Labeled "4" on encoder
 const int dialPin4 = 12; // Labeled "8" on encoder
 
+int readingsX[NUM_READINGS];  // the readings from the analog input
+int readingsY[NUM_READINGS];  // the readings from the analog input
+int readIndex = 0;  // the index of the current reading
+int totalX = 0;  // the running total
+int totalY = 0;  // the running total
+int averageX = 0;  // the average
+int averageY = 0;  // the average
+
 void setup() {
+  for (int thisReading = 0; thisReading < NUM_READINGS; thisReading++) {
+    readingsX[thisReading] = 0;
+    readingsY[thisReading] = 0;
+  }
+
   delay(1000);
   Serial.begin(9600);
   pinMode(pinButton, INPUT_PULLUP);
@@ -22,9 +40,26 @@ void setup() {
 }
 
 void loop() {
-  // Read joystick and button values
-  int16_t xAxis = analogRead(pinXAxis);
-  int16_t yAxis = analogRead(pinYAxis);
+  // subtract the last reading:
+  totalX = totalX - readingsX[readIndex];
+  totalY = totalY - readingsY[readIndex];
+  // read from the sensor:
+  readingsX[readIndex] = readAxis(pinXAxis);
+  readingsY[readIndex] = readAxis(pinYAxis);
+  // add the reading to the total:
+  totalX = totalX + readingsX[readIndex];
+  totalY = totalY + readingsY[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+  // if we're at the end of the array...
+  if (readIndex >= NUM_READINGS) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+  // calculate the average:
+  averageX = totalX / NUM_READINGS;
+  averageY = totalY / NUM_READINGS;
+
   int8_t zAxis = 0;
   bool buttonPressed = !digitalRead(pinButton);
 
@@ -36,13 +71,13 @@ void loop() {
   zAxis = 8*(1-dialVal4) + 4*(1-dialVal3) + 2*(1-dialVal2) + 1-dialVal1; // thrust
 
   // Scale analog readings to gamepad range (-32767 to 32767)
-  xAxis = map(xAxis, 0, 1023, -32767, 32767);
-  yAxis = map(yAxis, 0, 1023, -32767, 32767);
+  // xAxis = map(xAxis, 0, 1023, -32767, 32767);
+  // yAxis = map(yAxis, 0, 1023, -32767, 32767);
   zAxis = map(zAxis, 0, 15, 0, 127);
 
   // Update gamepad state
-  Gamepad.xAxis(xAxis);
-  Gamepad.yAxis(yAxis);
+  Gamepad.xAxis(averageX);
+  Gamepad.yAxis(averageY);
   Gamepad.zAxis(zAxis);
   if (buttonPressed) {
     Gamepad.press(1);  // Press button 1
@@ -54,13 +89,26 @@ void loop() {
   Gamepad.write();
 
   Serial.print("x = ");
-  Serial.print(xAxis);
+  Serial.print(averageX);
   Serial.print(", y = ");
-  Serial.print(yAxis);
+  Serial.print(averageY);
   Serial.print(", z (thrust) = ");
   Serial.print(zAxis);
   Serial.println();
   
   // Add a small delay to make the sketch more responsive
   delay(10);
+}
+
+// readAxis with deadzone - deadzone keeps the values at 0 
+int readAxis(byte pin) {
+  int val = analogRead(pin);
+
+  if (val < deadRangeLow)
+    return map(val ,0, deadRangeLow, -32766, 0);
+
+  if (val > deadRangeHigh)
+    return map(val, deadRangeHigh, 1023, 0, 32767);
+
+  return 0;
 }
